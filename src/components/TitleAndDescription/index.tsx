6b3243882216d7d612b99d1fece87c1a7b11ea85
Haekal Kalipaksi <hkalipaksi@proton.me>
2024-06-11 16:06:03 +0700

    Fix visual bug in title and description

--- src/components/TitleAndDescription/index.tsx
+++ src/components/TitleAndDescription/index.tsx
@@ -1,94 +1,95 @@
 import React from "react";
-import { Image, Text, View } from "react-native";
+import {Image, Text, View} from "react-native";
 import {
-  AppStackNavigationProp,
-  AppStackParamList,
+    AppStackNavigationProp,
+    AppStackParamList,
 } from "../../navigation/interface";
-import { StackNavigationProp } from "@react-navigation/stack";
+import {StackNavigationProp} from "@react-navigation/stack";
 
 type Props = {
-  title: string;
-  description: string;
-  redirect?: (
-    navigation: StackNavigationProp<AppStackParamList, "Home">
-  ) => void;
-  navigation?: AppStackNavigationProp<"Home">;
-  isDyslexic?: boolean;
+    title: string;
+    description: string;
+    redirect?: (
+        navigation: StackNavigationProp<AppStackParamList, "Home">
+    ) => void;
+    navigation?: AppStackNavigationProp<"Home">;
+    isDyslexic?: boolean;
 };
 
 const TitleAndDescription: React.FC<Props> = ({
-  title,
-  description,
-  redirect,
-  navigation,
-  isDyslexic = false,
-}) => (
-  <View
-    style={{
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-    }}
-  >
+                                                  title,
+                                                  description,
+                                                  redirect,
+                                                  navigation,
+                                                  isDyslexic = false,
+                                              }) => (
     <View
-      style={{
-        gap: 8,
-        flexShrink: 1,
-      }}
+        style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+        }}
     >
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
-        <Text
-          style={{
-            fontSize: isDyslexic ? 22 : 12,
-            fontWeight: "bold",
-            color: isDyslexic ? "#203474" : "black",
-          }}
-        >
-          {title}
-        </Text>
-
-        {!isDyslexic && redirect && navigation && (
-          <Text
-            onPress={() => redirect(navigation)}
+        <View
             style={{
-              color: "#0055B8",
-              fontWeight: "bold",
-              fontSize: 12,
+                gap: 8,
+                flexShrink: 1,
             }}
-          >
-            Lihat Semua
-          </Text>
+        >
+            <View
+                style={{
+                    flexDirection: "row",
+                    width: "100%",
+                    justifyContent: "space-between",
+                }}
+            >
+                <Text
+                    style={{
+                        fontSize: isDyslexic ? 22 : 12,
+                        fontWeight: "bold",
+                        color: isDyslexic ? "#203474" : "black",
+                    }}
+                >
+                    {title}
+                </Text>
+
+                {!isDyslexic && redirect && navigation && (
+                    <Text
+                        onPress={() => redirect(navigation)}
+                        style={{
+                            color: "#0055B8",
+                            fontWeight: "bold",
+                            fontSize: 12,
+                        }}
+                    >
+                        Lihat Semua
+                    </Text>
+                )}
+            </View>
+            <Text
+                style={{
+                    fontSize: isDyslexic ? 18 : 10,
+                    color: isDyslexic ? "#203474" : "black",
+                }}
+            >
+                {description}
+            </Text>
+        </View>
+        {isDyslexic && (
+            <View
+                style={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                    backgroundColor: "#46B54C",
+                    borderRadius: 10,
+                    width: 37,
+                    height: 37,
+                }}
+            >
+                <Image source={require("../../../assets/right-arrow.png")}/>
+            </View>
         )}
-      </View>
-      <Text
-        style={{
-          fontSize: isDyslexic ? 18 : 10,
-          color: isDyslexic ? "#203474" : "black",
-        }}
-      >
-        {description}
-      </Text>
     </View>
-    {isDyslexic && (
-      <View
-        style={{
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "#46B54C",
-          borderRadius: 10,
-          width: 37,
-          height: 37,
-        }}
-      >
-        <Image source={require("../../../assets/right-arrow.png")} />
-      </View>
-    )}
-  </View>
 );
 
 export default TitleAndDescription;
--- src/screens/HomeScreen/sections/Berita.tsx
+++ src/screens/HomeScreen/sections/Berita.tsx
@@ -1,32 +1,50 @@
 import {Image, ScrollView, Text, View} from "react-native";
-import TitleAndDescription from "../../../components/TitleAndDescription";
+import React from "react";
+import {StackNavigationProp} from "@react-navigation/stack";
+import {useQuery} from "@tanstack/react-query";
+import {ActivityIndicator} from "react-native-paper";
 import {
     AppStackNavigationProp,
     AppStackParamList
 } from "../../../navigation/interface";
-import React from "react";
-import {StackNavigationProp} from "@react-navigation/stack";
+import {ArticleList, fetchArticleList} from "../../../lib/api/article";
+import {useRefreshOnFocus} from "../../../hooks/useRefreshOnFocus";
+import TitleAndDescription from "../../../components/TitleAndDescription";
 
 type props = {
     navigation: AppStackNavigationProp<"Home">;
 };
 
 const Berita: React.FC<props> = ({navigation}) => {
-    const handleRedirect = (navigation: StackNavigationProp<AppStackParamList>) => {
-        navigation.navigate('Berita');
+
+    let {isPending, error, data, refetch} = useQuery<ArticleList[], Error>({
+        queryKey: ["articleList"],
+        queryFn: fetchArticleList,
+    })
+
+    useRefreshOnFocus(refetch)
+
+    const handleRedirect = (
+        navigation: StackNavigationProp<AppStackParamList>
+    ) => {
+        navigation.navigate("Berita");
     };
 
     return (
-        <View style={{
-            paddingLeft: 20,
-            paddingRight: 20,
-            gap: 12,
-        }}>
-
-            <TitleAndDescription title={"Berita MyMRTJ"} description={"Buat" +
-                " kamu yang ingin tau informasi terbaru tentang MRT"}
-                                 redirect={handleRedirect}
-                                 navigation={navigation}
+        <View
+            style={{
+                paddingLeft: 20,
+                paddingRight: 20,
+                gap: 12,
+            }}
+        >
+            <TitleAndDescription
+                title={"Berita MyMRTJ"}
+                description={
+                    "Buat" + " kamu yang ingin tau informasi terbaru tentang MRT"
+                }
+                redirect={handleRedirect}
+                navigation={navigation}
             />
 
             <ScrollView
@@ -37,45 +55,57 @@ const Berita: React.FC<props> = ({navigation}) => {
                     style={{
                         flexDirection: "row",
                         gap: 14,
-                    }}
-                >
-                    <View style={{
-                        flexDirection: "column",
-                        display: "flex",
-                        width: 250,
-                        gap: 14,
                         borderRadius: 10,
                         overflow: "hidden",
                         borderColor: "#E5EDF7",
                         borderWidth: 1,
-                    }}>
-                        <Image
-                            source={require("../../../../assets/menu-mrt/1.png")}
-                            style={{
-                                width: "100%",
-                                height: 125,
-                                objectFit: "cover",
-                            }}
-                        />
+                    }}
+                >
+                    {
+                        isPending ? <ActivityIndicator/> : (
+                            data && data.length > 0 ? (
+                                data.map((item, index) => (
+                                    <View style={{
+                                        flexDirection: "column",
+                                        display: "flex",
+                                        width: 250,
+                                        gap: 14,
+                                        borderRadius: 10,
+                                        overflow: "hidden",
+                                        borderColor: "#E5EDF7",
+                                        borderWidth: 1,
+                                    }} key={index}>
+                                        <Image
+                                            source={
+                                                {uri: item.headerUrl}
+                                            }
+                                            style={{
+                                                width: "100%",
+                                                height: 150,
+                                                objectFit: "cover",
+                                            }}
+                                        />
+                                        <View style={{
+                                            padding: 10,
+                                        }}>
+                                            <Text numberOfLines={2}
+                                                  ellipsizeMode={"tail"}>
+                                                {item.title}
+                                            </Text>
 
-                        <View style={{
-                            padding: 10,
-                        }}>
-                            <Text numberOfLines={2} ellipsizeMode={"tail"}>
-                                Per 25 Mei, Pembangunan Fase 2A CP 201 Capai
-                                77,36
-                                Persen aslkdjfslkdfjslkfalkjsa
-                            </Text>
+                                        </View>
 
-                        </View>
-
-                    </View>
+                                    </View>))
+                            ) : (
+                                <Text>Tidak ada berita yang
+                                    tersedia</Text>
+                            )
+                        )
+                    }
                 </View>
             </ScrollView>
-
-
         </View>
-    )
-}
+    );
+};
 
-export default Berita
+export default Berita;
