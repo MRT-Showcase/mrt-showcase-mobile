1d7ffa4c7f32df8eb8421ccc4f8e25908d0fc38c
Arxeno <rmfbkw04@gmail.com>
2024-06-11 11:41:45 +0700

    add logout button

--- src/screens/ProfileScreen/index.tsx
+++ src/screens/ProfileScreen/index.tsx
@@ -2,6 +2,7 @@ import { View, Text, ScrollView } from "react-native";
 import Hero from "./sections/Hero";
 import MenuProfil from "./sections/MenuProfil";
 import Umum from "./sections/Umum";
+import { Button } from "react-native-paper";
 
 const ProfileScreen = () => (
   <ScrollView>
@@ -19,6 +20,22 @@ const ProfileScreen = () => (
       >
         <MenuProfil />
         <Umum />
+        <Button
+          style={{
+            backgroundColor: "#FAE5EA",
+            borderRadius: 10,
+          }}
+        >
+          <Text
+            style={{
+              color: "#DD0000",
+              fontSize: 16,
+              fontWeight: "bold",
+            }}
+          >
+            Keluar
+          </Text>
+        </Button>
       </View>
     </View>
   </ScrollView>
